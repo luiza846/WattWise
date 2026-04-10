162afae2fc78ca4c57e4561a35d403a610f092f9
Ana Luiza <analuiza.siqueira61@gmail.com>
2026-04-09 21:44:45 -0300

    Add tela CriarConta
    Tela CriarConta criada e modificacoes na tela Main (login)

--- app/src/main/java/com/example/wattwise/CriarConta.java
+++ app/src/main/java/com/example/wattwise/CriarConta.java
@@ -1,8 +1,6 @@
 package com.example.wattwise;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class CriarConta extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_criar_conta);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void telaCadasElet(View view) {
-        Intent in = new Intent(MainActivity.this, CadastroEletrodomestico.class);
-        startActivity(in);
     }
 }
